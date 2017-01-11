476e17bc798317afdca0407b3b2f35ff8c9027b8
Cameron Houser <Cameron Houser>
2017-01-11 11:56:40 -0500

    added some strings

---
+++ 0.03_strings.js
@@ -0,0 +1,46 @@
+var firstName = "Cameron";
+var age = 37;
+var message = firstName + ": " + 55
+
+console.log(typeof message);
+console.log(typeof age);
+
+// quotes?
+
+var greetings = 'Kenn says "greeting\'s"';
+console.log(greetings);
+
+// string methods
+console.log(greetings.length);
+console.log(greetings.toUpperCase());
+console.log(greetings.toLowerCase());
+
+console.log(greetings.split(" "));
+console.log(greetings.slice(1));
+
+//Gold
+
+var age = 6
+if (age > 18 && age <= 30) {
+	console.log("You are an adult");
+}
+else if (age > 30 ) {
+	console.log("You are very old");
+}
+else {
+	console.log("You are a young pup");
+}
+
+//Silver
+
+var carType = "Truck";
+var carModel = "Ford 250";
+var carYear = "1987";
+var message = "I drive" + " " + carYear + " " + carModel + " " + carType;
+console.log(message);
+
+//Bronze
+var schoolName = "Living School";
+var gradDate = 1999;
+var message = "I graduated from" + " " + schoolName + " " + "in"+ " " + gradDate;
+console.log(message);

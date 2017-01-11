a59e1e143adc13cf8bdb65010b17d76065e6a99c
Cameron Houser <Cameron Houser>
2017-01-11 11:13:32 -0500

    added info on integers and variables

---
+++ 0.01_integers.js
@@ -0,0 +1,26 @@
+var world = "Hello World";
+console.log(world);
+/*
+console.log(1.2 * 5); // anything after . = is a float
+console.log(56 - 12);
+console.log(12 / 2);
+console.log(12 % 5);
+*/
+
+//bronze 
+console.log(37);
+console.log(2010);
+console.log("sisters =",2);
+console.log("brothers =",6);
+
+//Silver 
+console.log(3%2);
+console.log(5%3);
+console.log(30%18);
+
+//GOLD 
+console.log(Math.round(5*3.4));
+console.log(Math.pow(5,3));
+console.log(3e1); // 3 x 10
+console.log(3e3); // 3 x 10 x 10 x 10
+
---
+++ 0.02_variables.js
@@ -0,0 +1,70 @@
+/*
+// variable = x+y  is like G = x+y in math
+// name variable with a name that matches what is inside
+// variables with stings 
+var firstName =  "Cameron"
+var lastName = "Houser"
+var fullName = firstName + " " + lastName
+//concatenation (putting to variables building or combining strings)
+console.log(fullName);
+
+//empty variables
+/*var num 
+
+num = 3
+console.log(num); + " " 
+
+console.log(num);
+var num ;
+
+// empty string
+
+var petsName = "";
+petsName = "gus" ;
+console.log(petsName);
+
+// camel case (petsName) - first word is lower cased the next words are upper
+var packersCanWin = 1;
+//snake case - each word is connected with an _ 
+var packers_will_lose = 1;
+
+
+let 
+
+if (true) {
+	let petsName = ""
+}
+*/
+
+//1 + 1 = 2
+//console.log(1 += 1); // computer sees this as 1 = 1 + 1
+//console.log(age += 1); // computer sees this as age = age + 1
+//console.log(age /= 2);
+
+//short hand operators 
+
+//Gold 
+var age = 37;
+var deathAge = 97;
+var snacksPerDay = 2;
+var daysYear = 365;
+
+var yearSnacks = daysYear * snacksPerDay;
+var remainingLife = deathAge - age;
+var totalSnacks = remainingLife * yearSnacks;
+var youNeed = "You will need";
+var snacksStatement = "snacks to last you until the ripe old age of";
+var message = youNeed +" "+ totalSnacks + " "+  snacksStatement +" "+ deathAge;
+
+console.log(message);
+
+//silver 
+var myNumber = 0
+myNumber += 10
+console.log(myNumber);
+myNumber *= 9
+console.log(myNumber);
+myNumber /=7
+console.log(myNumber);
+
+
